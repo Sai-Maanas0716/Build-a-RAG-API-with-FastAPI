73d85053ffd315b65dc6fed984c2be33b39ae43a
Sai Maanas <saimaanas166@gmail.com>
2026-01-31 23:58:15 +0530

    add API functionalities

---
+++ embed.py
@@ -0,0 +1,10 @@
+import chromadb
+
+client = chromadb.PersistentClient(path="./db")
+collection = client.get_or_create_collection("docs")
+
+with open("k8s.txt", "r") as file:
+    text = file.read()
+
+collection.add(documents=[text], ids=["k8s"])
+print("documents are loaded into the database along with their embeddings indexed.")
